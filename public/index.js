ca61568671639aacfc1e24ea06b44834122c9511
Darklones <jimwei6@gmail.com>
2019-09-14 17:02:11 -0700

    Added index.html and set up index.js dependencies and browserwindow

--- public/index.js
+++ public/index.js
@@ -1,2 +1,14 @@
 const electron = require('electron')
+const url = require('url')
+const path = require('path')
 
+const {app, BrowserWindow} = electron
+
+let mainWindow
+
+app.on('ready',()=>{
+
+    mainWindow = new BrowserWindow({})
+
+    
+})
