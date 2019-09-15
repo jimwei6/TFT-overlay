f784551ea7f3e4bdc80eae0f33237354f772999b
Darklones <jimwei6@gmail.com>
2019-09-14 17:20:46 -0700

    Added defaults for the browser window setUp and a dummy message for html

--- public/index.js
+++ public/index.js
@@ -8,7 +8,21 @@ let mainWindow
 
 app.on('ready',()=>{
 
-    mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({
+        transparent: true,
+        frame: false,
+        width: 'screenX',
+        height: 'screenY',
+        title: "LoL tft Overlay",
+        backgroundColor: '#111111',
+        titleBarStyle: "hiddenInset",
+        webPreferences: {nodeIntegration: true}, 
 
-    
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true,
+    }))
 })
