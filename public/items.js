adde8961809277afc42cbcf8342dfcf7d5fa284e
Darklones <jimwei6@gmail.com>
2019-12-06 19:34:34 -0800

    added method for main horiz

--- public/items.js
+++ public/items.js
@@ -16,15 +16,34 @@ const path = __dirname.replace("public", "resources/itemIcons")
 function initMain(item){
     var mImg = document.createElement('img')
     mImg.setAttribute("class", 'itemIcon')
-    mImg.src = path + "/" + itemName + '.png'
+    mImg.src = path + "/" + item.name + '.png'
     
     var subImg = document.createElement('img')
     subImg.setAttribute("class", 'itemIcon')
-    subImg.src = path + "/" + itemName + '.png'
+    subImg.src = path + "/" +  "B._F._Sword" + ".png"
 
     var subImg2 = document.createElement('img')
     subImg2.setAttribute("class", 'itemIcon')
-    subImg2.src =  path + "/" + itemName + '.png'
+    subImg2.src =  path + "/" + formItems(item, BASEITEMS[0]).name + '.png'
+
+    HCLONE.getElementById('h-main').appendChild(mImg)
+    HCLONE.getElementById('h-main').appendChild(subImg)
+    HCLONE.getElementById('h-main').appendChild(subImg2)
     
 }
+
+function formItems(baseItem, baseItem2){
+    if(subBaseItem.itemNum < baseItem.itemNum)
+    {
+        forgedItem = mainItems[baseItem2.itemNum][Math.abs(baseItem2.itemNum - baseItem.itemNum)]
+    }
+    else
+    {
+        forgedItem = mainItems[baseItem.itemNum][baseItem2.itemNum - baseItem.itemNum]
+    }
+    return forgedItem
+
+}
+
+initMain(BASEITEMS[0])
 document.getElementById("info-bar").appendChild(HCLONE)
